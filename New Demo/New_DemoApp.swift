1ead1988b124dcc3578bae1aa4749792005bf107
mansapatel111 <142267357+mansapatel111@users.noreply.github.com>
2025-02-07 16:00:40 -0500

    Initial Commit

---
+++ New Demo/New_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_DemoApp.swift
+//  New Demo
+//
+//  Created by Mansa Patel on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct New_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ New DemoTests/New_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  New_DemoTests.swift
+//  New DemoTests
+//
+//  Created by Mansa Patel on 2/7/25.
+//
+
+import Testing
+@testable import New_Demo
+
+struct New_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
